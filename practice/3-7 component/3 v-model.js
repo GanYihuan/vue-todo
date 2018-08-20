37871fec74706e61f2abc55a7dd636d8a61f3246
GanYihuan <583520052@qq.com>
2018-08-21 02:32:10 +0800

    3-9 Vue 的组件之自定义双向绑定

--- practice/3-7 component/3 v-model.js
+++ practice/3-7 component/3 v-model.js
@@ -2,17 +2,21 @@ import Vue from 'vue'
 
 const component = {
   model: {
+    /* 自定义 prop 名称 */
     prop: 'value1',
     event: 'change'
   },
+  // props: ['value'],
   props: ['value1'],
   template: `
     <div>
+      <!-- <input type="text" @input="handleInput" :value="value"> -->
       <input type="text" @input="handleInput" :value="value1">
     </div>
   `,
   methods: {
-    handleInput (e) {
+    handleInput(e) {
+      // this.$emit('input', e.target.value)
       this.$emit('change', e.target.value)
     }
   }
@@ -23,13 +27,15 @@ new Vue({
     CompOne: component
   },
   el: '#root',
-  data () {
+  data() {
     return {
       value: '123'
     }
   },
   template: `
     <div>
+      <!-- <comp-one :value="value" @input="value=arguments[0]"></comp-one> -->
+      // 上下等价
       <comp-one v-model="value"></comp-one>
     </div>
   `

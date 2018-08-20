55f2c800430dddf463be118ba1aa0d13ddee5e79
GanYihuan <583520052@qq.com>
2018-08-21 05:37:33 +0800

    4-4 Vue-router 之导航守卫

--- practice/4-1 router/index.js
+++ practice/4-1 router/index.js
@@ -7,23 +7,32 @@ import './assets/styles/global.styl'
 Vue.use(VueRouter)
 const router = createRouter()
 
+/* Routing guard, data verify, route change will invoked */
 router.beforeEach((to, from, next) => {
-  console.log('before each invoked')
+  console.log('1. before each invoked')
   next()
   // if (to.fullPath === '/app') {
+  /* route jump, can write router-view define attribute */
   //   next({ path: '/login' })
   // } else {
   //   next()
   // }
 })
 
+/* Routing guard */
+/*
+2. routes.js/beforeEnter()
+3. todo.vue/beforeRouteEnter()
+*/
+
 router.beforeResolve((to, from, next) => {
-  console.log('before resolve invoked')
+  console.log('4. before resolve invoked')
   next()
 })
 
+/* Routing guard, trigger after jump */
 router.afterEach((to, from) => {
-  console.log('after each invoked')
+  console.log('5. after each invoked')
 })
 
 new Vue({
---
+++ practice/4-1 router/todo.vue
@@ -0,0 +1,27 @@
+﻿<script>
+export default {
+	/* Routing guard */
+	/* Trigger before route entry. Can't use this, vm alternative, set props=true */
+	beforeRouteEnter(to, from, next) {
+		console.log('3. todo before enter', this) // this => undefined
+		next(vm => {
+			console.log('after enter vm.id is ', vm.id)
+		})
+	},
+	/* Routing guard */
+	/* The same component is shown to trigger on different routes */
+	/* example: transfter route parameter */
+	beforeRouteUpdate(to, from, next) {
+		console.log('todo update enter')
+		next()
+	},
+	/* Routing guard */
+	beforeRouteLeave(to, from, next) {
+		console.log('todo leave enter')
+		// if (global.comfirm('are you sure leave this page?')) {
+		//   next()
+		// }
+		next()
+	}
+}
+</script>

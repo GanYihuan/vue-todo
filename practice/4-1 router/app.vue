d7e070b131151ddf8883fa87d181abd7ce2e2a11
GanYihuan <583520052@qq.com>
2018-08-21 04:56:20 +0800

    4-3 Vue-router 之路由参数传递

--- practice/4-1 router/app.vue
+++ practice/4-1 router/app.vue
@@ -9,46 +9,20 @@
     <transition name="fade" mode="out-in">
       <router-view/>
     </transition>
-    <!-- <router-view name="a"/> -->
+    <router-view name="a"/>
   </div>
 </template>
 
 <script>
 export default {
 	/* Get routing parameters */
-	// props: ['id'],
+	props: ['id'],
 	mounted() {
 		/* routes.js Setting props: true, Pass routing parameters :id */
 		/* Get routing parameters */
-		// console.log(this.id)
+		console.log(this.id)
 		/* Get routing information */
-		// console.log(this.$route)
-		/* get vuex store */
-		// console.log(this.$store)
-		// let i = 1
-		/* dispatch: trigger vuex/actions */
-		// this.$store.dispatch(updateCountAsync, {
-		//   num: 5,
-		//   time: 2000
-		// })
-		// this.updateCountAsync({
-		//   num: 5,
-		//   time: 2000
-		// })
-		// this['a/updateText']('123')
-		/* invoked vuex/actions */
-		// this['a/add']()
-		// this['b/testAction']()
-		// this.testAction()
-		// this.$store.state.count = 3
-		// setInterval(() => {
-		/* commit: invoked vuex/mutations */
-		//   this.$store.commit('updateCount', i ++)
-		//   this.updateCount({
-		//     num: i++,
-		//     num2: 2
-		//   })
-		// }, 1000)
+		console.log(this.$route)
 	}
 }
 </script>
--- practice/4-1 router/routes.js
+++ practice/4-1 router/routes.js
@@ -1,5 +1,5 @@
 ﻿// import Todo from '../views/todo/todo.vue'
-// import Login from '../views/login/login.vue'
+import Login from '../views/login/login.vue'
 
 export default [
   /* default route jump */
@@ -9,20 +9,20 @@ export default [
   },
   {
     /* When the route matches successfully, Will place the component in <router-view></router-view> */
-    path: '/app',
+    // path: '/app',
     /* transfer router parameter, matches route: /app/xxx */
-    // path: '/app/:id',
+    path: '/app/:id',
     /* transfer router parameter ':id' to <RankList/>, <RankList/> can use props get :id, decoupling $route */
     props: true,
-    /* two other ways of writing */
+    /* pass route params id=456 */
     // props: {
     //   id: '456'
     // },
     // props: (route) => ({ id: route.query.b }),
-    /* router-view add inside name=a, correspond to different interfaces */
+    /* router-view add inside name=a */
     // components: {
     //   default: Todo,
-    /* app.js: <!-- <router-view name="a"/> --> */
+    //   /* app.js: <router-view name="a"/> */
     //   a: Login
     // },
     component: () => {
@@ -30,8 +30,9 @@ export default [
       /* webpackChunkName: "todo-view" */
       import('../views/todo/todo.vue')
     },
+    /* <router-link :to="{name: 'app'}">app</router-link> */
     name: 'app',
-    /* 保存路由信息, 有利于SEO */
+    /* 保存路由信息, 有利于 SEO */
     meta: {
       title: 'this is app',
       description: 'asdasd'
@@ -40,14 +41,14 @@ export default [
     beforeEnter(to, from, next) {
       console.log('2. app route before enter')
       next()
-    }
+    },
     /* router-view 放置在 todo 组件里面显示子路由 */
-    // children: [
-    //   {
-    //     path: 'test',
-    //     component: Login
-    //   }
-    // ]
+    children: [
+      {
+        path: 'test',
+        component: Login
+      }
+    ]
   },
   {
     // path: '/login/exact',

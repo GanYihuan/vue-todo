fe3885016d19e7a2e4acee3eaad01d3c950308f1
GanYihuan <583520052@qq.com>
2018-09-15 10:56:46 +0800

    format

--- build/upload.js
+++ build/upload.js
@@ -1,18 +1,11 @@
 const qiniu = require('qiniu')
 const fs = require('fs')
 const path = require('path')
-
 const cdnConfig = require('../app.config').cdn
-
-const {
-  ak, sk, bucket
-} = cdnConfig
-
+const { ak, sk, bucket } = cdnConfig
 const mac = new qiniu.auth.digest.Mac(ak, sk)
-
 const config = new qiniu.conf.Config()
 config.zone = qiniu.zone.Zone_z0
-
 const doUpload = (key, file) => {
   const options = {
     scope: bucket + ':' + key
@@ -22,21 +15,25 @@ const doUpload = (key, file) => {
   const putPolicy = new qiniu.rs.PutPolicy(options)
   const uploadToken = putPolicy.uploadToken(mac)
   return new Promise((resolve, reject) => {
-    formUploader.putFile(uploadToken, key, file, putExtra, (err, body, info) => {
-      if (err) {
-        return reject(err)
+    formUploader.putFile(
+      uploadToken,
+      key,
+      file,
+      putExtra,
+      (err, body, info) => {
+        if (err) {
+          return reject(err)
+        }
+        if (info.statusCode === 200) {
+          resolve(body)
+        } else {
+          reject(body)
+        }
       }
-      if (info.statusCode === 200) {
-        resolve(body)
-      } else {
-        reject(body)
-      }
-    })
+    )
   })
 }
-
 const publicPath = path.join(__dirname, '../public')
-
 // publicPath/resource/client/...
 const uploadAll = (dir, prefix) => {
   const files = fs.readdirSync(dir)

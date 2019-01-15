37fbad40fc4cc7888f20ef91e2ee6237d18896ca
GanYihuan <583520052@qq.com>
2019-01-15 10:23:07 +0800

    instance

--- practice/3-2 instance/index.js
+++ practice/3-2 instance/index.js
@@ -39,13 +39,13 @@ setInterval(() => {
   /* a = 5 -> 10 -> 15 ... */
   app.obj.a = i /* obj 没定义 a 就赋值, 形成非响应式 */
   app.$forceUpdate() /* 强制组件渲染一次, 不建议使用 */
-  app.$set(app.obj, 'a', i) /* 设置值 */
+  app.$set(app.obj, 'a', i) /* 设置值 响应式 */
   app.$delete(app.obj, 'a', i) /* 删除值 */
 }, 1000)
 
 console.log(app.$data) /* text, object */
 console.log(app.$props) /* undefined */
-console.log(app.$el) /* <div>0</div> */
+console.log(app.$el) /* <div>0</div> html 节点 */
 console.log(app.$options) /* new Vue 传入的整个对象 */
 /* 下次有值改变时候渲染 */
 app.$options.render = h => {
@@ -56,7 +56,7 @@ console.log(app.$children)
 console.log(app.$slots) /* 插槽 */
 console.log(app.$scopedSlots) /* 插槽 */
 console.log(app.$refs) /* 定位组件, 节点 */
-console.log(app.$isServer)
+console.log(app.$isServer) // 服务端渲染
 
 /* app.$watch 要手动注销, 相当于 watch(){}, 差别在于是否能主动注销, 该方法要手动注销 */
 const unWatch = app.$watch('text', (newText, oldText) => {

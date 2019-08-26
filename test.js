b98fd64020723aa61b1d1e890141769944d11d61
GanYihuan <583520052@qq.com>
2019-08-27 00:30:40 +0800

    f

--- test.js
+++ test.js
@@ -1,16 +1,17 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: GanEhank
+ * @Date: 2019-03-22 04:51:07
+ * @LastEditors: GanEhank
+ * @LastEditTime: 2019-08-21 23:22:45
+ */
 beforeCreate()
 created()
 beforeMount()
-render()
-renderError()
-errorCaptured()
 mounted()
 beforeUpdate()
 updated()
-activated()
-deactivated()
 beforeDestory()
 destoryed()
-
-
-
+errorCaptured()
